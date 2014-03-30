04045b3eecc7519bea0186f4c645ed10869f195c
William Yaworsky <wj.px01@gmail.com>
2014-03-30 01:46:51 -0400

    Rewrote 2 in scala

--- 2/scala/2.scala
+++ 2/scala/2.scala
@@ -2,19 +2,13 @@ import euler._
 
 object Main extends App {
 
-  def fibSum(sumWhen: Int => Boolean, stopWhen: Int => Boolean, sum: Int = 0, cur: Int = 0): Int = {
-    var curVal = euler.fib(cur)
-    if (stopWhen(curVal)) sum
-    else
-      if (sumWhen(curVal)) fibSum(sumWhen, stopWhen, sum + curVal, cur + 1)
-      else fibSum(sumWhen, stopWhen, sum, cur + 1)
-  }
+  def fibSum(sum: Int = 0, cur: Int = 1, prev: Int = 1, until: Int = 100, when: (Int) => Boolean = x =>  true): Int =
+    if      (cur > until) sum
+    else if  (when(cur))  fibSum(sum + cur, prev + cur, cur, until, when)
+    else                  fibSum(sum      , prev + cur, cur, until, when)
 
-  val sum = fibSum(
-    sumWhen  = (x) => x % 2 == 0,
-    stopWhen = (x) => x > 4000000
-  )
+  val answer = fibSum(until = 4000000, when = x => x % 2 == 0)
 
-  println(sum)
+  println(answer)
 
 }
