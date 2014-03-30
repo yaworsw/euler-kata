ebe8ca96cd4e227a55c53bf5b25d9d38957c7c1a
William Yaworsky <wj.px01@gmail.com>
2014-03-29 23:30:22 -0400

    Rewrote 1 in scala

--- 1/scala/1.scala
+++ 1/scala/1.scala
@@ -2,18 +2,8 @@ import euler._
 
 object Main extends App {
 
-  type Set = Int => Boolean
+  val answer = (1 until 1000).filter((x) => x % 3 == 0 || x % 5 == 0).foldLeft(0)(_+_)
 
-  val maximum = 999
-
-  def reduce(s: Set, res: Int = 0, cur: Int = 0): Int =
-    if (cur > maximum) res
-    else if (s(cur)) reduce(s, res + cur, cur + 1)
-    else reduce(s, res, cur + 1)
-
-  def set(x: Int): Boolean =
-    x % 3 == 0 || x % 5 == 0
-
-  println(reduce(set))
+  println(answer)
 
 }
